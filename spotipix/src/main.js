11bf5424234a0503685101d0b3cf5deacd9e3e81
ananyaaa-18 <ananyaarana.18@gmail.com>
2025-10-18 12:51:52 +0530

    added basic JS functionality to make the buttons more responsivee

--- spotipix/src/main.js
+++ spotipix/src/main.js
@@ -1,18 +1,11 @@
-const { invoke } = window.__TAURI__.core;
+document.getElementById("playPauseBtn").addEventListener("click", () => {
+  alert("Play/Pause clicked 🎵");
+});
 
-let greetInputEl;
-let greetMsgEl;
+document.getElementById("nextbtn").addEventListener("click", () => {
+  alert("Next Song ⏭️");
+});
 
-async function greet() {
-  // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-  greetMsgEl.textContent = await invoke("greet", { name: greetInputEl.value });
-}
-
-window.addEventListener("DOMContentLoaded", () => {
-  greetInputEl = document.querySelector("#greet-input");
-  greetMsgEl = document.querySelector("#greet-msg");
-  document.querySelector("#greet-form").addEventListener("submit", (e) => {
-    e.preventDefault();
-    greet();
-  });
+document.getElementById("prevBtn").addEventListener("click", () => {
+  alert("Previous Song ⏮️");
 });
