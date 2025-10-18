401e517325fc9ac57e88e7077b05c977cec9265a
ananyaaa-18 <ananyaarana.18@gmail.com>
2025-10-18 14:08:43 +0530

    extracting access token from URL and redirecting user

--- spotipix/src/spotifyAuth.js
+++ spotipix/src/spotifyAuth.js
@@ -4,4 +4,16 @@ const scopes = [
     "user-read-currently-playing",
     "user-read-playback-state",
     "user-modify-playback-state"
-].at.join(" ");
+].join(" ");
+
+export function loginToSpotify() {
+    const authUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&redirect_uri=${encodeURIComponent(redirectUri)}&scope=${encodeURIComponent(scopes)}`;
+    window.location.href = authUrl;
+}
+
+export function getAccessTokenFromUrl() {
+    const hash = window.location.hash;
+    const params = new URLSearchParams(hash.substring(1));
+    return params.get("access_token");
+}
+
