672112f6bdf48394f368790a47a975fc90f1cb1c
ananyaaa-18 <ananyaarana.18@gmail.com>
2025-10-19 16:12:46 +0530

    + more stuff

--- spotipix/vite.config.js
+++ spotipix/vite.config.js
@@ -1,12 +1,11 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  root: "src",
-  base: "./",
+  root: "./src",
   build: {
     outDir: "../dist",
   },
   server: {
-    port: 5173,
+    port: 1420,
   },
 });
